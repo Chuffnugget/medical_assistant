1e18cb53c84fc4375966cf30d8ce52e39fe4a35a
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2025-02-20 23:03:11 +0000

    Create const.py

---
+++ custom_components/medical_assistant/const.py
@@ -0,0 +1,3 @@
+"""const.py - Constants for Medical Assistant integration."""
+DOMAIN = "medical_assistant"
+DAYS_OF_WEEK = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
